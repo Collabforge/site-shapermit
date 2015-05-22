72657156ccdc56355b0b34a097aae9ac25b594af
MichaelWheldon <michael.wheldon@gmail.com>
2015-05-22 17:06:00 +1000

    New commit, no message

--- themes/flip_theme/templates/page.tpl.php
+++ themes/flip_theme/templates/page.tpl.php
@@ -160,10 +160,10 @@ jQuery(document).ready(function($) {
             <li class="dropdown">
                 <a href="#" data-toggle="dropdown" class="dropdown-toggle">Admin <b class="caret"></b></a>
                 <ul class="dropdown-menu">
-                    <li><a href="http://shapermitl/profile"><i class="icon-cog">&nbsp;</i>Profile</a></li>
-                    <li><a href="http://shapermitl/email_preferences"><i class="icon-envelope">&nbsp;</i>Email Settings</a></li>
-                    <li><a href="http://shapermitl/help"><i class="icon-star">&nbsp;</i>Help</a></li>
-                    <li><a data-method="delete" href="http://shapermitl/users/sign_out"><i class="icon-share">&nbsp;</i>Sign Out</a></li>
+                    <li><a href="https://shapermitl/profile"><i class="icon-cog">&nbsp;</i>Profile</a></li>
+                    <li><a href="https://shapermitl/email_preferences"><i class="icon-envelope">&nbsp;</i>Email Settings</a></li>
+                    <li><a href="https://shapermitl/help"><i class="icon-star">&nbsp;</i>Help</a></li>
+                    <li><a data-method="delete" href="https://shapermitl/users/sign_out"><i class="icon-share">&nbsp;</i>Sign Out</a></li>
                 </ul>
             </li>
         </ul>
