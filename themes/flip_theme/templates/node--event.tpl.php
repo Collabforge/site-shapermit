9914f543adde2b861dadb90c435c5e848d843ffd
MichaelWheldon <michael.wheldon@gmail.com>
2015-05-27 16:58:04 +1000

    New commit, no message

--- themes/flip_theme/templates/node--event.tpl.php
+++ themes/flip_theme/templates/node--event.tpl.php
@@ -100,7 +100,7 @@
   <div class="span9 rmit-title">
               <?php print render($title_prefix); ?>
           
-               <h1<?php print $title_attributes; ?>><i class="icon-chevron-right"></i> <?php print $title; ?></h1>
+               <h1<?php print $title_attributes; ?>> <?php print $title; ?></h1>
                  <?php /* print '<br/>Posted by ' . $name . $user_picture; */ ?>
           
               <?php print render($title_suffix); ?>
