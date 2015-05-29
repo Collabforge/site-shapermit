0e1cc74027020883fcd9fcf12f7f8281fce75f6e
Matt Withoos <mattwithoos@gmail.com>
2015-05-29 12:31:04 +1000

    created template file for var overwrite

---
+++ themes/flip_theme/template.php
@@ -0,0 +1,10 @@
+<?php
+
+function flip_theme_preprocess_page(&$vars) {
+  $vars['mobile']['block_search'] = _meta_structure_block_view('search', 'form');
+  $menu_profile = _meta_structure_block_view('menu', 'menu-profile-menu');
+  $menu_secondary = _meta_structure_block_view('menu', 'menu-meta-secondary-menu');
+  $menu_primary = _meta_structure_block_view('menu', user_is_logged_in() ? 'menu-main-menu---auth' : 'main-menu-menu-anon');
+  $vars['mobile']['block_menu'] = $menu_primary;
+  $vars['mobile']['block_profile'] = $menu_profile;
+}
