7a2af2c2741d9fcf1230418974196cd8286acf94
MichaelWheldon <michael.wheldon@gmail.com>
2015-06-17 11:23:29 +1000

    New commit, no message

--- themes/flip_theme/template.php
+++ themes/flip_theme/template.php
@@ -8,3 +8,15 @@ function flip_theme_preprocess_page(&$vars) {
   $vars['mobile']['block_menu'] = $menu_secondary;
   $vars['mobile']['block_profile'] = $menu_profile;
 }
+
+function open_framework_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $form['search_block_form']['#title_display'] = 'invisible';
+    $form['search_block_form']['#attributes']['class'][] = 'input-medium search-query';
+    $form['search_block_form']['#attributes']['placeholder'] = t('Search this site...');
+    $form['actions']['submit']['#attributes']['class'][] = 'btn-search';
+    $form['actions']['submit']['#attributes']['alt'] = 'Click to search';  
+    $form['actions']['submit']['#type'] = 'image_button';
+    $form['actions']['submit']['#src'] = drupal_get_path('theme', 'open_framework') . '/images/searchbutton.png';
+  }
+}
