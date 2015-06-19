001b9b902582b9f43060dba268ac3f732a6b109f
MichaelWheldon <michael.wheldon@gmail.com>
2015-06-19 14:52:17 +1000

    New commit, no message

--- themes/flip_theme/templates/page.tpl.php
+++ themes/flip_theme/templates/page.tpl.php
@@ -46,14 +46,14 @@ if ((($user->uid) && ($page['admin_shortcuts'])) || (($user->uid) && ($secondary
 
   <div id="tab-container" class="hide-mobile-menu-titles tab-container">
 
-      <div class="tab_logo tab"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">Mobile Navigation starts here.</a></div>
+      <div class="tab_logo tab"><a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home">Navigation starts here.</a></div>
     <ul class="etabs">
-      <li class="tab"><a href="#tabs1-menu" class="icon-reorder icon-2x"><p>Mobile Navigation Dropdown Site Navigation</p></a></li>
-      <li class="tab"><a href="#tabs1-search" class="icon-search icon-search icon-2x"><p>Mobile Navigation Dropdown Search</p></a></li>
-      <li class="tab"><a href="#tabs1-profile" class="icon-user icon-2x"><p>Mobile Navigation Dropdown Profile</p></a></li>
+      <li class="tab"><a href="#tabs1-menu" class="icon-reorder icon-2x"><p>Skip - Mobile Navigation Dropdown Menu</p></a></li>
+      <li class="tab"><a href="#tabs1-search" class="icon-search icon-search icon-2x"><p>Skip - Mobile Navigation Dropdown Search</p></a></li>
+      <li class="tab"><a href="#tabs1-profile" class="icon-user icon-2x"><p>Skip - Mobile Navigation Dropdown Profile</p></a></li>
     </ul>
 
-    <div class="ui-tab-content" id="tabs1-menu">
+    <div id="skip-to-navigation" class="ui-tab-content" id="tabs1-menu">
       <?php print $mobile['block_menu']; ?>
     </div>
 
